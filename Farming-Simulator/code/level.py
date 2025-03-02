947514c85b3a8e17351bbd491a7c07ba82da1008
monotai <wannwattqarl@gmail.com>
2025-03-02 21:22:16 +0700

    first commit

---
+++ Farming-Simulator/code/level.py
@@ -0,0 +1,18 @@
+import pygame
+from settings import *
+
+
+class Level:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+
+        self.all_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+
+
+    def run(self, dt):
+        self.display_surface.fill('black')
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update(dt)
---
+++ Farming-Simulator/code/main.py
@@ -0,0 +1,26 @@
+import pygame, sys
+from settings import *
+from level import *
+
+class Game: # our class for game running
+	def __init__(self):
+		pygame.init() # initaily
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) # screen size
+		pygame.display.set_caption('Sprout land') # caption
+		self.clock = pygame.time.Clock() # clock
+		self.level = Level()
+
+	def run(self): # run game
+		while True: # loop
+			for event in pygame.event.get(): # get event on sceen
+				if event.type == pygame.QUIT: # click on quit button
+					pygame.quit() # quit game
+					sys.exit() # stop program
+  
+			dt = self.clock.tick() / 1000 # fram rate
+			self.level.run(dt)
+			pygame.display.update() # diplay changing like move, ...
+
+if __name__ == '__main__': # first running ?
+	game = Game() # defind instand
+	game.run() # run game
